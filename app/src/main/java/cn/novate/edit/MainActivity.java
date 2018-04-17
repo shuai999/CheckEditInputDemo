434a6a3232b851f6ed1d2101a3d906198a4ae32d
chenershuai <chenershuaizg@163.com>
2018-04-17 08:22:48 +0800

    first commit

---
+++ app/src/main/java/cn/novate/edit/MainActivity.java
@@ -0,0 +1,81 @@
+package cn.novate.edit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText et_usertel;
+    private EditText et_password;
+    private Button btn_login;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        et_usertel = (EditText) findViewById(R.id.et_usertel);
+        et_password = (EditText) findViewById(R.id.et_password);
+        btn_login = (Button) findViewById(R.id.btn_login);
+
+        // 监听多个输入框
+        TextChange textChange = new TextChange();
+        et_usertel.addTextChangedListener(textChange);
+        et_password.addTextChangedListener(textChange);
+        // if user changed, clear the password
+        et_usertel.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+//                et_password.setText(null);
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+    }
+
+
+
+    // EditText监听器
+    class TextChange implements TextWatcher {
+
+        @Override
+        public void afterTextChanged(Editable arg0) {
+
+        }
+
+        @Override
+        public void beforeTextChanged(CharSequence arg0, int arg1, int arg2,
+                                      int arg3) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence cs, int start, int before,
+                                  int count) {
+
+            boolean Sign2 = et_usertel.getText().length() > 0;
+            boolean Sign3 = et_password.getText().length() > 0;
+
+            if (Sign2 & Sign3) {
+                btn_login.setEnabled(true);
+            }
+            // 在layout文件中，对Button的text属性应预先设置默认值，否则刚打开程序的时候Button是无显示的
+            else {
+                btn_login.setEnabled(false);
+            }
+        }
+
+    }
+}
